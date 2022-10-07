20abf7c13e4076c25a674509c270f546d09c06de
Andrea Criollo <113551844+acriollo1@users.noreply.github.com>
2022-10-07 19:47:23 +0000

    trying to do dice with words

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
  # random
-from random import randint
+from random import randint, choice
   # Python comes with a built in random library. There are a lot of functions included in this random library, so we will only 
   #show you two useful functions for now.
 # print("random")
 # first_random = randint(1, 50)
 # print(first_random)
-dice1 = randint(1,6)
-dice2 = randint(1,6)
-dice3 = randint(1,6)
-dice4 = randint(1,6)
-dice5 = randint(1,6)
+# dice1 = randint(1,6)
+# dice2 = randint(1,6)
+# dice3 = randint(1,6)
+# dice4 = randint(1,6)
+# dice5 = randint(1,6)
 
-my_gamble = dice1 + dice2 + dice3 + dice4 + dice5
-print(my_gamble)
+# my_gamble = dice1 + dice2 + dice3 + dice4 + dice5
+# print(my_gamble)
 
-if my_gamble == 8:
-  print("Roll again")
-elif my_gamble % 2 == 1 :
-  print("You almost won")
-elif my_gamble > 12 and my_gamble < 18:
-  print("You lost")
-else:
-  print("You won")
+# if my_gamble == 8:
+#   print("Roll again")
+# elif my_gamble % 2 == 1 :
+#   print("You almost won")
+# elif my_gamble > 12 and my_gamble < 18:
+#   print("You lost")
+# else:
+#   print("You won")
 
+dictionary1 = {} #empty dictionary  
+color = ['blue', 'red', 'green', 'turquiso', 'purple']
+my_random = choice(color)
+print(my_random)
+my_characters = ['rogue', 'elf', 'knight', 'ninja', 'samurai']
+for character in my_characters:
+  if my_gamble == character[0]:
+    print(character)
+  else:
+    print(my_gamble)
+    
   # from random import shuffle
   # # This shuffles the list "in-place" meaning it won't return
   # # anything, instead it will effect the list passed
